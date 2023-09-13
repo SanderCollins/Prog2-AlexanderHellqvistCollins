dc217b6810a3e20862d20d78ea3956f6f13aeb4b
SanderCollins <alexander.hellqvistcollins@skola.taby.se>
2023-09-13 10:10:18 +0200

    chill

---
+++ 13-9 Metoder/Add_this.py
@@ -0,0 +1,9 @@
+def add_this(*numbers):
+    sum = 0
+    for x in numbers:
+         sum += x
+    print(sum)
+
+add_this(1, 2, 3, 4, 5)
+
+add_this(1, 2, 3)
---
+++ 13-9 Metoder/mjolk.py
@@ -0,0 +1,6 @@
+def food(mat, vegan = False):
+    if vegan == True:
+        mat = "sojamjölk"
+    print(mat)
+
+food("mjölk")
